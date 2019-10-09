a32a25aa5edd6096ad62c092acb6a81b41f6aa16
Ian Foster <ifoster2@cnm.edu>
2019-10-09 16:24:05 -0600

    updated  more text for interaction-flow

--- epic/interaction-flow.php
+++ epic/interaction-flow.php
@@ -11,12 +11,12 @@
 <h1>Interaction Flow</h1>
 
 <ul>
-	<li>Login to facebook page for Salon</li>
-	<li> Finds InJoy U Salon Page </li>
+	<li>Login to InJoy U Salon</li>
+	<li> Sign  up by  entering personal information</li>
 	<li>User clicks book now icon</li>
-	<li> User is redirected to InJoyU Salon Official web-site</li>
 	<li>Click on an open time in Calender to book appointment</li>
-	<li>Submit apointment</li>
+	<li>Submit anointment</li>
+	<li>User will recieve a confirmation email within 12hrs</li>
 </ul>
 <a href="index.php">Back Home</a>
 </body>
