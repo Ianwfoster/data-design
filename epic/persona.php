88a5a0d041a2913a48feb0d9745f914efa0e7935
Ian Foster <ifoster2@cnm.edu>
2019-10-09 15:52:59 -0600

    made <br> and moved back home link

--- epic/persona.php
+++ epic/persona.php
@@ -9,19 +9,21 @@
 <body>
 
 <h1>PERSONA</h1>
-<ol>
-	<li>Rene is a proud owner of her Salon.</li>
-	<li>She is an outgoing person that always has a bright smile on her face.</li>
-	<li>Sometimes, customer cancel causing Rene to have a few unproductive hours witch frustrates her.</li>
-	<li>She is a big facebook user, and likes to post the hair styles that been done on the clients.</li>
-	<li>She had a website built and wants to get more use out of it.</li>
-	<li>Her hobbies are cooking and writing poetry.</li>
-</ol>
+
+		<p>Rene is a proud owner of her Salon.<br>
+	She is an outgoing person that always has a bright smile on her face.<br>
+			Rene uses facebook alot, and  she <em>loves</em> to post the hair styles that been done on the clients.<br>
+	In her spare time she likes to pick up a  good book to read, do a little cooking and writing poetry.<br>
+			Rene also likes to travel. She has been to 8 different countries and more than half of the U.S.<br>
+			Sometimes, customer cancel causing Rene to have a few unproductive hours witch frustrates her.<br>
+			However she stays thinking positive, because even with the cancelations she still does awsome overall<br>
+			She had a website built and wants to get more use out of it.<br></p>
+
 
 
 <a href="index.php">Back Home</a><br>
 
-<img src="InJoyU.jpg" alt="pic of InjoyU website"><br>
+<img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="pic of pretty-smile"><br>
 
 </body>
 </html>
