cc8488da242f5c59b6983e403f6c61129e403ccd
Ian Foster <ifoster2@cnm.edu>
2019-10-09 14:51:22 -0600

    updted link to pic

--- epic/persona.php
+++ epic/persona.php
@@ -17,7 +17,7 @@
 	<li>Has very own website.</li>
 	<li>Customers go to her facebook page to request an appointment.</li>
 </ol>
-<img src="https://www.facebook.com/EnJ.OTR/photos/a.589853767701776/62193943449320" alt="pic of Rene"><br>
+<img src="https://www.facebook.com/EnJ.OTR/photos/a.589853767701776/621939434493209/?type=3&eid=ARB9nDjN_WxTtIO9e8o5muuZ0INTiDMuMWpY-NQ3jxXAZT98WkdZid1Q0uGkUBSIslJGKOCluFfW3oXw&__xts__%5B0%5D=68.ARAhqgSHE7LkWj34tJyjZHulo-diUlLRWLrFyIr1Wt7YI3GFZdTjE385HBri07EyxSN3rhxos7SE7HyrxOGu1yC0FRrM-GaPVeDm0sYeFKyQvRLC1Q5sIf91udL362dWdF78BKtgl2nnArkLsf0J2TOa5JD2jwFnoDelGxYg2puFJsmjSrFcHtl7a-CLfpP7K1cfKKhXcBjkMy3CeXer7CfgoVw1ZLy6zq7eJX1vR6XX4MQs1XGq2UAPAirdCNrTkEIiEWNwRSdoQUrXLY06TaIyx-MUL8b6ZWCj0-1hL5DOkHPAv9R1CYwcbya4FpDYq_8&__tn__=EHH-R" alt="pic of Rene"><br>
 <a href="index.php">Back Home</a>
 </body>
 </html>
