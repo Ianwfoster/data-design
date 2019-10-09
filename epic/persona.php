d768db902b22ad9ba9b39001733db84b82732e6e
Ian Foster <ifoster2@cnm.edu>
2019-10-09 15:55:29 -0600

    made <br> and moved back home link

--- epic/persona.php
+++ epic/persona.php
@@ -23,7 +23,7 @@
 
 <a href="index.php">Back Home</a><br>
 
-<img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="pic of pretty-smile"><br>
+<img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="pic of pretty-smile">
 
 </body>
 </html>
