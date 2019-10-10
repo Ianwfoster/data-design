162dbda08f5162a4023ff0833056a6d694fdba18
Ian Foster <ifoster2@cnm.edu>
2019-10-09 20:34:34 -0600

    added punctuation

--- epic/interaction-flow.php
+++ epic/interaction-flow.php
@@ -10,20 +10,20 @@
 <body>
 <h1>Booking an Appointment</h1>
 
-<p>Example of making an appointment.</p>
+<h2>Example of making an appointment.</h2>
 
 <p>Name: Rene</p>
-<p>Pre-Condition: Rene's Customer logged in to InjoyU Salon and signed up as a user<br>
+<p>Pre-Condition: Rene's Customer logged in to InjoyU Salon and signed up as a user.<br>
 </p>
 <p> Post-Condition: Rene's Customer makes appointment right from her smart phone.<br>
 </p>
 
 <ul>
 	<li>Login to InJoy U Salon</li>
-	<li> Sign  up by  entering personal information</li>
-	<li>User clicks book now icon</li>
-	<li>Click on an open time in Calender to book appointment</li>
-	<li>Submit anointment</li>
+	<li> Sign  up by  entering personal information.</li>
+	<li>User clicks book now icon.</li>
+	<li>Click on an open time in Calender to book appointment.</li>
+	<li>Submit anointment.</li>
 	<li>User will receive a confirmation email within 30 minutes.</li>
 </ul>
 <a href="index.php">Back Home</a>
